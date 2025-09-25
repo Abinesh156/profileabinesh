3e7fe28b4e32cc21bfeac994a1b31548be062cb5
abinesh <abinesh.s@althisolutions.com>
2025-09-26 00:00:11 +0530

    just updated typing running code

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Traveler", "Developer", "Blogger", "Designer", "Engineer"],
+        strings: ["Traveler", "Cloud Engineer", "Blogger", "Backend Developer", "Engineer","Freelancer","DevOps Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Traveler", "Developer", "Blogger", "Designer", "Engineer"],
+        strings: ["Traveler", "Cloud Engineer", "Blogger", "Backend Developer", "Engineer","Freelancer","DevOps Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
